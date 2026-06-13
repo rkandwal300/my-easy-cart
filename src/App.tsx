7bccb664646ebb71dee8a4570c3177057de5368e
Rahul Kandwal <rahulkandwal707@gmail.com>
2026-06-13 17:03:40 +0530

    fix(App): Remove dead code and added suspense booundary

--- src/App.tsx
+++ src/App.tsx
@@ -1,82 +1,13 @@
-// import React, { createContext, useContext, useEffect, useState } from "react";
-// import Header from "./files/Header";
-// import Footer from "./files/footer";
-// import ProductListPage from "./files/PdList/ProductListPage";
-// import ProductDetail from "./files/PdDetail/ProductDetail";
-// import Error from "./files/Error";
-// import { Routes, Route } from "react-router-dom";
-// import Loading, { Contact } from "./files/Loading";
-// import LogIn2 from "./files/SignUp/LogIn2";
-// import CartP from "./files/Cart/CartP";
-// import SignUp from "./files/SignUp/Sign_Up";
-// import AuthRoute from "./files/SignUp/AuthRoute"; 
-// import UserState, { UserContext } from "./Api/UserContext";
-// import Authentication from "./files/SignUp/Authentication";
-// import CartState from './Api/CartContext';
-// import AlertState from "./Api/AlertContext";
-// import ProductState from "./Api/Sort-Search";
-
-// function App() {
- 
-
-//   return (
-//     <div>
-  
-//       <AlertState>
-//       <UserState>
-//       <ProductState >
-//       <CartState>
-//         {/* <EasyLogIn /> */}
-//         {/* <LogIn />     // with formik ki help se doobara bnao  */}
-//         <Header />
-
-
-//         {/* <Contact /> */}
-
-//         <Routes>
-//           <Route
-//             path="/products/:id"
-//             element={<ProductDetail  />}
-//           />
-//           <Route path="/Contact_Us" element={<Contact />} />
-//           <Route path="/products/Cart" element={<CartP />} />
-
-//           <Route path="/signup" element={<SignUp />} />
-//           <Route
-//             path="/LogIn"
-//             element={
-//               <AuthRoute children={<Authentication children={<LogIn2 />} />} children2 ={ <SignUp />} />
-//             }
-//           />
-
-//           {/* <Route index element={<UserRoute children={<ProductListPage />} />} /> */}
-
-//           <Route index element={<ProductListPage />} />
-
-//           <Route path="/:abc" element={<Error />} />
-//           <Route path="*" element={<Error />} />
-//         </Routes>
-
-//         <Footer />
-//       </CartState>
-//       </ProductState>
-//       </UserState>
-//         </AlertState>
-
-//     </div>
-//   );
-// }
-
-// export default App;
-
-
-
 import { RouterProvider } from "react-router-dom";
 import { router } from "./router/RouterProvider";
+import { Suspense } from "react";
+import Loading from "./components/shared/Loading";
 
 function App() {
   return (
-            <RouterProvider router={router} />
+    <Suspense fallback={<Loading />}>
+      <RouterProvider router={router} />
+    </Suspense>
   );
 }
 
